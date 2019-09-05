56ebd621d923ff1f1c513eb2ae8bfd6a6842f14c
Yanrong Xu <8xuyanrong@gmail.com>
2019-09-05 02:54:13 -0400

    fixed build error

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import ProjectCard from "./ProjectCard";
-import {StaticQuery, graphql, Link} from "gatsby"
-import {primary, black} from "../utilities"
+import {StaticQuery, graphql} from "gatsby"
+import {primary} from "../utilities"
 
 const PROJECT_LIST_QUERY = graphql`
   query ProjectListQuery {
@@ -36,42 +36,14 @@ const PROJECT_LIST_QUERY = graphql`
   }
 `;
 
-const PROJECT_LIST_QUERY_ALL = graphql`
-  query ProjectListQueryAll {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          excerpt
-          frontmatter {
-            title
-            path
-            description
-            mobile
-            links {
-              github
-              website
-            }
-            imgUrl {
-              childImageSharp {
-                fluid(maxWidth: 956) {
-                  src
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`;
 
-const Projects = (all) => (
+const Projects = () => (
   <StaticQuery
-    query={all ? PROJECT_LIST_QUERY_ALL : PROJECT_LIST_QUERY}
+    query={PROJECT_LIST_QUERY}
     render={({ allMarkdownRemark }) => (
       <StyledProjects id="projects">
         <SectionHeading>
-          {!all && <span>my featured</span>}Projects
+          Projects
         </SectionHeading>
         <ProjectsContainer>
           {allMarkdownRemark.edges.map((edge, index) => (
@@ -87,7 +59,6 @@ const Projects = (all) => (
             />
           ))}
         </ProjectsContainer>
-        {/*{!all && <Button to="/projects">View All Projects</Button>}*/}
       </StyledProjects>
     )}
   />
@@ -95,7 +66,6 @@ const Projects = (all) => (
 
 const StyledProjects = styled.section`
   padding-top: 50px;
-  margin-bottom: 3rem;
 `;
 
 const ProjectsContainer = styled.div`
@@ -112,40 +82,6 @@ const SectionHeading = styled.h2`
   span {
     color: ${primary};
     text-transform: lowercase;
-  }
-`;
-
-const ButtonContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 3rem;
-  margin-top: 5rem;
-`;
-
-const Button = styled(Link)`
-  border: 1px solid ${black};
-  border-radius: 4px;
-  padding: 0.5rem 1rem;
-  background: transparent;
-  cursor: pointer;
-  transition: all 0.2s;
-  font-size: 1.8rem;
-  font-weight: 300;
-  color: ${black};
-  text-decoration: none;
-  text-transform: uppercase;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 20%;
-  margin: auto;
-  &:visited {
-    color: ${black};
-  }
-  &:hover {
-    border-color: ${black};
-    color: ${primary};
-    border: 1px solid ${primary};
   }
 `;
 
